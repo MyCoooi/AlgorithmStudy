44247bcdf27a480cb8eb7beb8e85ca8c79cd5232
jiheon <wlgjs8474@naver.com>
2024-03-07 17:57:53 +0900

    2024-03-07 좋은 수열 문제 풀이 진행 중

---
+++ Beakjoon/src/W20240304/Main_골드4_2661_좋은수열.java
@@ -0,0 +1,16 @@
+package W20240304;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_골드4_2661_좋은수열 {
+
+	static int N;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(in.readLine());
+		
+		
+	}
+}
--- Beakjoon/src/W20240304/Main_실버2_15666_N과M12.java
+++ Beakjoon/src/W20240304/Main_실버2_15666_N과M12.java
@@ -2,34 +2,56 @@ package W20240304;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
-// fail
 public class Main_실버2_15666_N과M12 {
 	
 	static int N, M;
-	static boolean used [][];
 	static int [] inputNumbers, numbers ;
+	static ArrayList<Integer> removeDuplicatesInputNumbers;
 	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws Exception {
-		input();
-		// 오름차순 정렬(사전 순으로 증가하는 순서로 출력하기 위해)
-		Arrays.sort(inputNumbers);
-		
+		input(); // 수 입력받기
+		removeDuplicates(); // 입력받은 수 중 중복되는 수 제거
+		// 오름차순 정렬(사전 순으로 증가하는 순서로 수열을 출력하기 위함)
+		Collections.sort(removeDuplicatesInputNumbers);
 		// 순열 찾기
-		
-		
+		permutation(0, 0);
 		// 출력
 		System.out.println(sb);
 	}
 	
-	static void permutation(int depth, int prevN) {
+	static void permutation(int depth, int idx) {
 		// 기저 조건
-		
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(numbers[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
 		
 		// 유도부분
+		for (int i = idx; i < removeDuplicatesInputNumbers.size(); i++) {
+			numbers[depth] = removeDuplicatesInputNumbers.get(i);
+			permutation(depth+1, i);
+		}
 		
+	}
+	
+	static void removeDuplicates() {
+		for (int i = 0; i < N; i++) {
+			boolean flag = true;
+			for (int j = 0; j < removeDuplicatesInputNumbers.size(); j++) {
+				if (inputNumbers[i] == removeDuplicatesInputNumbers.get(j)) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) removeDuplicatesInputNumbers.add(inputNumbers[i]);
+		}
 	}
 	
 	static void input() throws Exception {
@@ -40,8 +62,8 @@ public class Main_실버2_15666_N과M12 {
 		M = Integer.parseInt(split[1]);
 		
 		inputNumbers = new int[N];
+		removeDuplicatesInputNumbers = new ArrayList<>();
 		numbers = new int[M];
-		used = new boolean[M][10001];
 		
 		split = in.readLine().split(" ");
 		for (int i = 0; i < N; i++) {
