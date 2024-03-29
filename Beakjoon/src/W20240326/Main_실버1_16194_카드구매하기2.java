0a284bc2d88fb9b71c8ba08da7fe1bf049210d45
jiheon <wlgjs8474@naver.com>
2024-03-29 17:56:04 +0900

    2024-03-29

--- Beakjoon/src/W20240326/Main_실버1_16194_카드구매하기2.java
+++ Beakjoon/src/W20240326/Main_실버1_16194_카드구매하기2.java
@@ -1,8 +1,26 @@
 package W20240326;
 
-public class Main_실버1_16194_카드구매하기2 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
+public class Main_실버1_16194_카드구매하기2 {
+	
+	static int N; // 구매하려고 하는 카드 개수
+	static int [] price; // 각 카드팩의 가격
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(in.readLine());
+		
+		price = new int[N + 1]; // 인덱스 0은 쓰지 않는다(카드는 1개부터 존재하므로)
+		
+		String split [] = in.readLine().split(" ");
+		for (int i = 1; i <= N; i++) {
+			price[i] = Integer.parseInt(split[i]);
+		}
+		
+		
 		
 	}
 }
