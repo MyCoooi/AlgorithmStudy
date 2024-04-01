8a2cd808c6da63e7885a65dac8bf4f534d4ac518
jiheon <wlgjs8474@naver.com>
2024-04-01 11:15:33 +0900

    2024-04-01

---
+++ Beakjoon/src/W20240401/Main_골드5_5430_AC.java
@@ -0,0 +1,21 @@
+package W20240401;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_골드5_5430_AC {
+
+	static int N;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(in.readLine());
+		for (int tc = 1; tc <= T; tc++) {			
+			String cmd = in.readLine();
+			N = Integer.parseInt(in.readLine());
+		}
+		
+		
+	}
+}
