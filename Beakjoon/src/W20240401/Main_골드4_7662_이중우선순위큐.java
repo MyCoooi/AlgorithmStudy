968870c1b0480d850d4e3f6f6fa6ecc34b2aeaa6
jiheon <wlgjs8474@naver.com>
2024-04-04 17:39:06 +0900

    2024-04-04

--- Beakjoon/src/W20240401/Main_골드4_7662_이중우선순위큐.java
+++ Beakjoon/src/W20240401/Main_골드4_7662_이중우선순위큐.java
@@ -16,6 +16,86 @@ public class Main_골드4_7662_이중우선순위큐 {
 	
 	static int K; // 연산의 개수
 	
+	static class Tree {
+		Node root, min, max;
+		// get
+		public Integer getMin() {
+			if (min == null) return null;
+			return min.value;
+		}
+		public Integer getMax() {
+			if (max == null) return null;
+			return max.value;
+		}
+		// delete
+		public void deleteMin() {	
+			if (min.cnt > 1) min.cnt -= 1;
+			else {
+				if (min == max) min = max = null;
+				Node parent = min.rightParent;
+				parent.left = min.right;
+			}
+		}
+		public void deleteMax() {
+			if (max.cnt > 1) max.cnt -= 1;
+			else {
+				if (min == max) min = max = null;
+				Node parent = max.leftParent;
+				parent.right = max.left;
+				
+			}
+		}
+		// insert
+		public void insert(int value) {
+			if (this.isEmpty()) {
+				root = new Node(value);
+				min = root;
+				max = root;
+				return;
+			}
+			
+			Node target = root;
+			while(true) {
+				if (target.value == value) {
+					target.cnt += 1;
+					return;
+				}
+				else if (target.value < value) {
+					if (target.right == null) {
+						target.right = new Node(value);
+						target.right.leftParent = target;
+						if (max == target) max = target.right;
+						return;
+					}
+					else target = target.right;
+				}
+				else { // value < target.value 
+					if (target.left == null) {
+						target.left = new Node(value);
+						target.left.rightParent = target;
+						if (min == target) min = target.left;
+						return;
+					}
+					else target = target.left;
+				}
+			}
+		}
+		// isEmpty
+		public boolean isEmpty() {
+			return root == null;
+		}
+	}
+	
+	static class Node {
+		Node left, right, leftParent, rightParent;
+		int value, cnt; // cnt: 동일 정수가 삽입될 때 +1
+		public Node(int value) {
+			super();
+			this.value = value;
+			this.cnt = 1;
+		}
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -23,11 +103,25 @@ public class Main_골드4_7662_이중우선순위큐 {
 		int T = Integer.parseInt(in.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			K = Integer.parseInt(in.readLine());
+			
+			Tree doubleQ = new Tree();
+			
 			for (int i = 0; i < K; i++) {
 				String split [] = in.readLine().split(" ");
-				String cmd = split[0]; // 어떤 연산인지?
+				char cmd = split[0].charAt(0); // 어떤 연산인지?
 				int n = Integer.parseInt(split[1]); // 연산의 타겟값
+				
+				if (cmd == 'I') doubleQ.insert(n);
+				else if (cmd == 'D' && n == MAXVALUE) doubleQ.deleteMax();
+				else doubleQ.deleteMin();
 			}
+			
+			Integer min = doubleQ.getMin();
+			Integer max = doubleQ.getMax();
+			if (min == null && max == null) sb.append("EMPTY\n");
+			else sb.append(max + " " + min + "\n");
 		}
+		System.out.print(sb);
 	}
+	
 }
