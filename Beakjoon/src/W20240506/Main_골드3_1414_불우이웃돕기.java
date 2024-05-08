489a320f9ad8b8ba2597a8388434ed8430498921
jiheon <wlgjs8474@naver.com>
2024-05-08 17:58:41 +0900

    2024-05-08

---
+++ Beakjoon/src/W20240506/Main_골드3_1414_불우이웃돕기.java
@@ -0,0 +1,16 @@
+package W20240506;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_골드3_1414_불우이웃돕기 {
+	
+	static int N;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(in.readLine()); // 방의 갯수
+		
+	}
+}
