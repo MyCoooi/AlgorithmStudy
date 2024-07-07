f2450e69c39ce7e24df8dd1725ad35720cc065bc
jiheon <wlgjs8474@naver.com>
2024-07-08 00:05:19 +0900

    2024-07-08

--- Beakjoon/src/W20240628/Main_골드5_17396_백도어.java
+++ Beakjoon/src/W20240628/Main_골드5_17396_백도어.java
@@ -8,13 +8,14 @@ public class Main_골드5_17396_백도어 {
 	static int N, M;
 	static boolean [] canGo;
 	static Node [] graph;
-	static int answer;
-	static int times [];
+	static long answer;
+	static long times [];
 	
 	static class Node implements Comparable<Node> {
-		int to, time;
+		int to;
+		long time;
 		Node next;
-		public Node(int to, int time, Node next) {
+		public Node(int to, long time, Node next) {
 			super();
 			this.to = to;
 			this.time = time;
@@ -22,7 +23,7 @@ public class Main_골드5_17396_백도어 {
 		}
 		@Override
 		public int compareTo(Node o) {
-			return Integer.compare(this.time, o.time);
+			return Long.compare(this.time, o.time);
 		}
 	}
 	
@@ -36,12 +37,12 @@ public class Main_골드5_17396_백도어 {
 		
 		canGo = new boolean[N];
 		graph = new Node[N];
-		times = new int[N];
+		times = new long[N];
 		answer = -1;
 		
 		split = in.readLine().split(" ");
 		for (int i = 0; i < split.length; i++) {
-			canGo[i] = (Integer.parseInt(split[0]) == 1) ? false : true;
+			canGo[i] = (Integer.parseInt(split[i]) == 1) ? false : true;
 		}
 		// N - 1 번째 분기점(상대 넥서스)은 상대방의 시야에 보이지만 갈 수 있다.
 		canGo[N - 1] = true;
@@ -60,30 +61,33 @@ public class Main_골드5_17396_백도어 {
 		
 		dijkstra(0);
 		
-		System.out.println(times[N-1]);
+		System.out.println(answer);
 	}
 	
 	static void dijkstra(int startNode) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		boolean [] visited = new boolean[N];
 		
-		visited[0] = true;
 		pq.offer(new Node(startNode, 0, null));
-		Arrays.fill(times, Integer.MAX_VALUE);
+		Arrays.fill(times, Long.MAX_VALUE);
 		times[0] = 0;
 		while(!pq.isEmpty()) {
 			Node cur = pq.poll();
 			
 			if (visited[cur.to]) continue;			
 			visited[cur.to] = true;
+			if (cur.to == N - 1) break;
 			
 			for (Node iter = graph[cur.to]; iter != null; iter = iter.next) {
-				if (!canGo[iter.to]) continue;
-				if (!visited[iter.to] && times[iter.to] > times[cur.to] + times[iter.time] ) {
-					times[iter.to] = times[cur.to]  + times[iter.time];
+				if (visited[iter.to] || !canGo[iter.to]) continue;
+				if (times[iter.to] > times[cur.to] + iter.time ) {
+					times[iter.to] = times[cur.to]  + iter.time;
 					pq.offer(new Node(iter.to, times[iter.to], null));
 				}
 			}
+		}
+		if (visited[N-1]) {
+			answer = times[N-1];
 		}
 	}
 }
